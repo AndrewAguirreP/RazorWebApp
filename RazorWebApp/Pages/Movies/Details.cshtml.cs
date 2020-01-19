3be21e3fb6e1661207fb1740f37cc278f4846c87
Andrew Aguirre <seesharpprojects@hotmail.com>
2020-01-20 01:48:41 +0800

    Initial with Scaffolding

---
+++ RazorWebApp/Pages/Movies/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorWebApp.Data;
+using RazorWebApp.Models;
+
+namespace RazorWebApp
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RazorWebApp.Data.AppDbContext _context;
+
+        public DetailsModel(RazorWebApp.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Movie = await _context.Movie.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Movie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
