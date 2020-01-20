65d002c3d69b41afe744861b390e908179baf8b1
AndrewAguirreP <seesharpprojects@hotmail.com>
2020-01-21 02:13:17 +0800

    DataAnotations validation

--- RazorWebApp/Migrations/20200120181247_Update_DataAnotations.Designer.cs
+++ RazorWebApp/Migrations/20200120181247_Update_DataAnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorWebApp.Data;
 
 namespace RazorWebApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200120181247_Update_DataAnotations")]
+    partial class Update_DataAnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
